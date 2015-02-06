f911ea80f68fd846bfad0d05bfef26de784d4699
Casey Foster <c@sey.me>
2015-02-06 13:32:45 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# cogs-transformer-example
+# cogs-transformer-text
 
-An example transformer for [Cogs].
+A text transformer for [Cogs].
 
-[![Build Status]](http://travis-ci.org/caseywebdev/cogs-transformer-example)
+[![Build Status]](http://travis-ci.org/caseywebdev/cogs-transformer-text)
 
 [Cogs]: https://github.com/caseywebdev/cogs
-[Build Status]: https://secure.travis-ci.org/caseywebdev/cogs-transformer-example.png
+[Build Status]: https://secure.travis-ci.org/caseywebdev/cogs-transformer-text.png
--- test/test.js
+++ test/test.js
@@ -4,15 +4,14 @@ helper.run({
   'test/config.json': {
     'test/input.txt': {
       path: 'test/input.txt',
-      buffer: helper.getFileBuffer('test/output.txt'),
-      hash: helper.getFileHash('test/output.txt'),
+      buffer: helper.getFileBuffer('test/output.js'),
+      hash: helper.getFileHash('test/output.js'),
       requires: [{
         path: 'test/input.txt',
         hash: helper.getFileHash('test/input.txt')
       }],
       links: [],
       globs: []
-    },
-    'test/error.txt': Error
+    }
   }
 });
