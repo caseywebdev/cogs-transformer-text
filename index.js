a9b5c624528f2cd093feb92ef39fd91129a840cc
Casey Foster <c@sey.me>
2015-04-08 17:15:08 -0500

    Remove extra space

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var DEFAULTS = {
 };
 
 module.exports = function (file, options, cb) {
-  var source =  'export default ' + JSON.stringify(file.buffer.toString());
+  var source = 'export default ' + JSON.stringify(file.buffer.toString());
   options = _.extend({}, DEFAULTS, options);
   babel(_.extend({}, file, {buffer: new Buffer(source)}), options, cb);
 };
